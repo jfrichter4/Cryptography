52e2624036458682b71935baa97ba7e47198468e
jfrichter4 <42844330+jfrichter4@users.noreply.github.com>
2018-10-02 21:07:28 -0400

    Updated from Brython Server: 10/2/2018 9:07:32 PM

--- cryptography.py
+++ cryptography.py
@@ -10,3 +10,30 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+Question = print(input("Enter e to encrypt, d to decrypt, or q to quit: "))
+if Question == 'e':
+    print(...)
+    elif Question == 'd':
+        print(...)
+    elif Question == 'q':
+        print("Goodbye!")
+    
+Enter e to encrypt, d to decrypt, or q to quit: z
+Did not understand command, try again.
+Enter e to encrypt, d to decrypt, or q to quit: e
+Message: Two plus two = Five
+Key: Lorem ipsum
++KF;B(CH=NIZ}m;R\Dt
+Enter e to encrypt, d to decrypt, or q to quit: d
+Message: +KF;B(CH=NIZ}m;R\Dt
+Key: Lorem ipsum
+Two plus two = Five
+Enter e to encrypt, d to decrypt, or q to quit: q
+Goodbye!
+message =  ‘Hello, do you know my other computer is your computer?’ 
+translated =  ‘ ‘   
+i = len(message)  -  1   
+while  i >=  0 :   
+         translated = translated + message[i]      
+         i  =  i  =  1     
+print(translated)
