d9677a42f90d05ec3fa635c665b3b762ee5d4b4f
jfrichter4 <42844330+jfrichter4@users.noreply.github.com>
2018-10-03 12:30:31 -0400

    Updated from Brython Server: 10/3/2018 12:30:34 PM

--- cryptography.py
+++ cryptography.py
@@ -9,17 +9,21 @@ Write and submit a program that encrypts and decrypts user data.
 
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
+
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 Question = print(input("Enter e to encrypt, d to decrypt, or q to quit: "))
+thing = associations.reverse()
+zipcryp = zip(associations, thing)
 if Question == 'e':
-    print(...)
-    elif Question == 'd':
-        print(...)
-    elif Question == 'q':
-        print("Goodbye!")
-    else print(Two plus two = Five)
-    
-    
+    enput = print(input("Message:"))
+    print(enput)
+elif    Question == 'd':
+    print("Message:")
+elif    Question == 'q':
+    print("Goodbye!")
+else:
+    print("Did not understand command, try again.")
+"""
 Enter e to encrypt, d to decrypt, or q to quit: z
 Did not understand command, try again.
 Enter e to encrypt, d to decrypt, or q to quit: e
@@ -32,10 +36,11 @@ Key: Lorem ipsum
 Two plus two = Five
 Enter e to encrypt, d to decrypt, or q to quit: q
 Goodbye!
-message =  ‘Hello, do you know my other computer is your computer?’ 
-translated =  ‘ ‘   
+message =  print('Hello, do you know my other computer is your computer?')
+translated =  ' '   
 i = len(message)  -  1   
 while  i >=  0 :   
          translated = translated + message[i]      
          i  =  i  =  1     
 print(translated)
+"""
