593f7afde04efef3ebdfb64793f969a06d6a0da3
jfrichter4 <42844330+jfrichter4@users.noreply.github.com>
2018-10-14 10:55:55 -0400

    Updated from Brython Server: 10/14/2018 10:55:59 AM

--- cryptography.py
+++ cryptography.py
@@ -9,7 +9,7 @@ Write and submit a program that encrypts and decrypts user data.
 
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
-assc =  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]- = _+?!"
+assc = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 Straight = {}
 Reverse = {}
 while True:
@@ -69,78 +69,3 @@ while True:
     else:
         print("Did not understand command, try again.")
         continue
-
-"""
-import string
-associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
-straight = {}
-reverse = {}
-for j in range(0, len(associations)):
-    straight[associations[j]] = j
-    reverse[j] = associations[j]
-Question = str(input("Enter e to encrypt, d to decrypt, or q to quit: "))
-if Question == "e":
-    enput = str(input("Message:"))
-    enkey = str(input("Key: "))
-    outputen = ""
-    listput = list(enput)
-    listkey = list(enkey)
-    j = 0
-    while len(enput) > len(enkey):
-        enkey += enkey[j]
-        j += 1
-    for o in range(0, len(enput)):
-        if straight[enput[o]] + straight[enkey[o]] <= len(associations):
-                outputen += reverse[straight[enput[o]]] + straight[enkey[o]]
-        else:
-            print(outputen)
-else:
-    print("meh?")
-    
-
-thing = associations.reverse()
-zipcryp = zip(associations, thing)
-if Question == 'e':
-    enput = print(input("Message:"))
-    key = print(input("Key: "))
-    print(key)
-elif    Question == 'd':
-    print("Message:")
-elif    Question == 'q':
-    print("Goodbye!")
-else:
-    print("Did not understand command, try again.")
-Enter e to encrypt, d to decrypt, or q to quit: z
-Did not understand command, try again.
-Enter e to encrypt, d to decrypt, or q to quit: e
-Message: Two plus two = Five
-Key: Lorem ipsum
-+KF;B(CH=NIZ}m;R\Dt
-Enter e to encrypt, d to decrypt, or q to quit: d
-Message: +KF;B(CH=NIZ}m;R\Dt
-Key: Lorem ipsum
-Two plus two = Five
-Enter e to encrypt, d to decrypt, or q to quit: q
-Goodbye!
-message =  print('Hello, do you know my other computer is your computer?')
-translated =  ' '   
-i = len(message)  -  1   
-while  i >=  0 :   
-         translated = translated + message[i]      
-         i  =  i  =  1     
-print(translated)
-
-import cryptography
-from cryptography.fernet import Fernet
-key = Fernet.generate_key()
-cipher_suite = Fernet(key)
-cipher_text = cipher_suite.encrypt(b"A really secret message. Not for prying eyes.")
-plain_text = cipher_suite.decrypt(cipher_text)
-import cryptography
-from cryptography.fernet import Fernet
-key = Fernet.generate_key()
-cipher_suite = Fernet(key)
-cipher_text = cipher_suite.encrypt(b"A really secret message. Not for prying eyes.")
-plain_text = cipher_suite.decrypt(cipher_text)
-btw: https://docs.python-guide.org/scenarios/crypto/
-"""
